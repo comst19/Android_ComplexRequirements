5c1c92697ec41616a3657dc157d7db55b4431155
Minsu <comst19@naver.com>
2023-06-26 19:22:14 +0900

    보안 키패드 구현 완료

---
+++ FinancialServices/app/src/main/java/com/example/financialservices/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.financialservices
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.financialservices.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.view = this
+    }
+
+    fun openShuffle(){
+        startActivity(Intent(this, PinActivity::class.java))
+    }
+
+    fun openVerifyOtp(){
+
+    }
+
+
+}
