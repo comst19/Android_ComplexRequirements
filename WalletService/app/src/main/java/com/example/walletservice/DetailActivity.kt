5337a186c8d980461ede190a183d6daf0db88147
Minsu <comst19@naver.com>
2023-06-28 20:00:24 +0900

    MotionLayout 사용하여 카드 모음 UI

---
+++ WalletService/app/src/main/java/com/example/walletservice/DetailActivity.kt
@@ -0,0 +1,47 @@
+package com.example.walletservice
+
+import android.content.Context
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import com.example.walletservice.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.cardTitleTextView.text = intent.getStringExtra(CARD_NAME)
+        binding.cardLayout.backgroundTintList =
+            intent.getParcelableExtra(CARD_COLOR) as? ColorStateList
+
+    }
+
+    companion object {
+        private const val CARD_NAME = "CARD_NAME"
+        private const val CARD_COLOR = "CARD_COLOR"
+
+        fun start(
+            context: Context,
+            cardName: String,
+            cardColor: ColorStateList?,
+            optionsCompat: ActivityOptionsCompat
+        ) {
+            Intent(context, DetailActivity::class.java).apply {
+                putExtra(CARD_NAME, cardName)
+                putExtra(CARD_COLOR, cardColor)
+            }.run {
+                context.startActivity(this, optionsCompat.toBundle())
+            }
+        }
+    }
+}
