e5a9c81cc7c8191f5d5784b7b83d5405b9ccd268
comst19 <comst19@naver.com>
2023-09-14 23:55:43 +0900

    안드로이드 인스타그램 앱 만들기 - 구글 로그인 (Firebase)

--- Howlstagram/app/src/main/java/com/comst/howlstagram/login/LoginActivity.kt
+++ Howlstagram/app/src/main/java/com/comst/howlstagram/login/LoginActivity.kt
@@ -3,10 +3,13 @@ package com.comst.howlstagram.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.comst.howlstagram.R
 import com.comst.howlstagram.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -16,7 +19,6 @@ class LoginActivity : AppCompatActivity() {
     private val loginViewModel : LoginViewModel by viewModels()
 
     // 회원가입 관리 변수
-    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,7 +27,6 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel
         binding.activity = this
         binding.lifecycleOwner = this
-        auth = FirebaseAuth.getInstance()
         setObserve()
     }
 
@@ -44,19 +45,20 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun loginWithSignupEmail(){
-        println("Email")
-        auth.createUserWithEmailAndPassword(loginViewModel.id.value.toString(), loginViewModel.password.value.toString()).addOnCompleteListener {
-            if (it.isSuccessful){
-                loginViewModel.showInputNumberActivity.value = true
-            }else{
-                // 아이디가 있을 경우
-            }
-        }
-    }
+
 
     fun findId(){
         println("findId")
         loginViewModel.showFindIdActivity.value = true
+    }
+
+    // 구글 로그인이 성공한 결과값 받는 함수
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+        loginViewModel.firebaseAuthWithGoogle(account.idToken)
     }
 }
