e054874b27c0053985c2c93d33bff6c648b433bc
comst19 <comst19@naver.com>
2023-09-15 01:28:39 +0900

    안드로이드 인스타그램 앱 만들기 - 페이스북 로그인 (Firebase)

--- Howlstagram/app/src/main/java/com/comst/howlstagram/login/LoginActivity.kt
+++ Howlstagram/app/src/main/java/com/comst/howlstagram/login/LoginActivity.kt
@@ -1,22 +1,38 @@
 package com.comst.howlstagram.login
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.comst.howlstagram.R
 import com.comst.howlstagram.databinding.ActivityLoginBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginBehavior
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.Arrays
+
 
 class LoginActivity : AppCompatActivity() {
+
+    val TAG = "LoginActivity"
 
     private lateinit var binding : ActivityLoginBinding
 //    private lateinit var loginViewModel : LoginViewModel
     private val loginViewModel : LoginViewModel by viewModels()
+    private lateinit var callbackManager : CallbackManager
 
     // 회원가입 관리 변수
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +43,47 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel
         binding.activity = this
         binding.lifecycleOwner = this
+        callbackManager = CallbackManager.Factory.create()
         setObserve()
+        printHashKey(this)
+    }
+
+    fun loginFacebook(){
+        var loginManager = LoginManager.getInstance()
+        loginManager.loginBehavior = LoginBehavior.WEB_ONLY
+        loginManager.logInWithReadPermissions(this, Arrays.asList("email"))
+        loginManager.registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+            override fun onCancel() {
+            }
+
+            override fun onError(error: FacebookException) {
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                val token = result.accessToken
+                loginViewModel.firebaseAuthWithFacebook(token)
+            }
+
+        })
+    }
+
+    fun printHashKey(pContext: Context) {
+        try {
+            val info = pContext.packageManager.getPackageInfo(
+                pContext.packageName,
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey: String = String(Base64.encode(md.digest(), 0))
+                Log.i(TAG, "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e(TAG, "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e(TAG, "printHashKey()", e)
+        }
     }
 
     fun setObserve(){
@@ -60,5 +116,10 @@ class LoginActivity : AppCompatActivity() {
         val task = GoogleSignIn.getSignedInAccountFromIntent(data)
         val account = task.getResult(ApiException::class.java)
         loginViewModel.firebaseAuthWithGoogle(account.idToken)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 }
--- Howlstagram/app/src/main/java/com/comst/howlstagram/login/LoginViewModel.kt
+++ Howlstagram/app/src/main/java/com/comst/howlstagram/login/LoginViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.comst.howlstagram.R
+import com.facebook.AccessToken
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
@@ -50,6 +52,17 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     // 암호화 푸는 코드
     fun firebaseAuthWithGoogle(idToken : String?){
         val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                showInputNumberActivity.value = true
+            }else{
+                // 아이디가 있을 경우
+            }
+        }
+    }
+
+    fun firebaseAuthWithFacebook(accessToken: AccessToken){
+        val credential = FacebookAuthProvider.getCredential(accessToken.token)
         auth.signInWithCredential(credential).addOnCompleteListener {
             if (it.isSuccessful){
                 showInputNumberActivity.value = true
