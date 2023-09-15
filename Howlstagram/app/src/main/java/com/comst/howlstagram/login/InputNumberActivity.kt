b8b7b691a0e326405ab0bbd640a5ba73065f2bf5
comst19 <comst19@naver.com>
2023-09-15 17:10:06 +0900

    안드로이드 인스타그램 앱 만들기 - 아이디, 패스워드 찾기 디비 설계 (이메일 소유 인증)

--- Howlstagram/app/src/main/java/com/comst/howlstagram/login/InputNumberActivity.kt
+++ Howlstagram/app/src/main/java/com/comst/howlstagram/login/InputNumberActivity.kt
@@ -1,12 +1,34 @@
 package com.comst.howlstagram.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
 import com.comst.howlstagram.R
+import com.comst.howlstagram.databinding.ActivityInputNumberBinding
 
 class InputNumberActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityInputNumberBinding
+    val inputNumberViewModel : InputNumberViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_number)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_input_number)
+        binding.viewModel = inputNumberViewModel
+        binding.lifecycleOwner = this
+        setObserve()
+    }
+
+    fun setObserve(){
+        inputNumberViewModel.nextPage.observe(this){
+            if (it){
+                finish()
+                // 이메일 인증이 된 사용자만 로그인 가능
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
     }
 }
