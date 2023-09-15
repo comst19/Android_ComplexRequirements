91b917ce9aa6d74d345db0ac07820dc5e3328dde
comst19 <comst19@naver.com>
2023-09-15 17:52:14 +0900

    안드로이드 인스타그램 앱 만들기 - 아이디, 패스워드 찾기 구현(이메일에서 패스워드 초기화)

--- Howlstagram/app/src/main/java/com/comst/howlstagram/login/FindIdActivity.kt
+++ Howlstagram/app/src/main/java/com/comst/howlstagram/login/FindIdActivity.kt
@@ -2,11 +2,30 @@ package com.comst.howlstagram.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.comst.howlstagram.R
+import com.comst.howlstagram.databinding.ActivityFindIdBinding
 
 class FindIdActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFindIdBinding
+    val findIdViewModel : FindIdViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_find_id)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_find_id)
+        binding.activity = this
+        binding.viewModel = findIdViewModel
+        binding.lifecycleOwner = this
+        setObserve()
+    }
+
+    fun setObserve(){
+        findIdViewModel.toastMessage.observe(this) {
+            if (it.isNotEmpty()){
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
