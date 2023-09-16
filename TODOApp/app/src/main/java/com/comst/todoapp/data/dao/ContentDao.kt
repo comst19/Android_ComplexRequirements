c8b3270976b04f308b48f7fbdbc1e639392941cc
comst19 <comst19@naver.com>
2023-09-16 19:10:34 +0900

    TODO 앱 - ROOM 연결 (Hilt 사용)

---
+++ TODOApp/app/src/main/java/com/comst/todoapp/data/dao/ContentDao.kt
@@ -0,0 +1,9 @@
+package com.comst.todoapp.data.dao
+
+import androidx.room.Dao
+
+@Dao
+interface ContentDao {
+
+
+}
---
+++ TODOApp/app/src/main/java/com/comst/todoapp/data/di/DaoModule.kt
@@ -0,0 +1,17 @@
+package com.comst.todoapp.data.di
+
+import com.comst.todoapp.data.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DaoModule {
+
+    @Singleton
+    @Provides
+    fun providesContentDao(appDatabase: AppDatabase) = appDatabase.contentDao()
+}
